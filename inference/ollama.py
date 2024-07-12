6588532d1003513847546fe89291baffe2a842b6
Jeomon George <127664852+Jeomon@users.noreply.github.com>
2024-07-12 18:52:32 +0530

    Update ollama.py
    included stream method for both completion and chat completion api

--- inference/ollama.py
+++ inference/ollama.py
@@ -1,9 +1,11 @@
 from inference.base import BaseInference
 from message.base import AIMessage
 from requests import post
+from json import loads
+from typing import Literal
 
 class ChatOllama(BaseInference):
-    def invoke(self,messages: list[dict])->AIMessage:
+    def invoke(self,messages: list[dict],stream=False,format:Literal['','json']='')->AIMessage:
         headers=self.headers
         temperature=self.temperature
         url=self.base_url or "http://localhost:11434/api/chat"
@@ -13,8 +15,8 @@ class ChatOllama(BaseInference):
             "options":{
                 "temperature": temperature,
             },
-            "format":"json",
-            "stream":False
+            "format":format,
+            "stream":stream
         }
         try:
             response=post(url=url,json=payload,headers=headers)
@@ -24,3 +26,68 @@ class ChatOllama(BaseInference):
         except Exception as err:
             print(err)
     
+    def stream(self,messages: list[dict],format:Literal['','json']=''):
+        headers=self.headers
+        temperature=self.temperature
+        url=self.base_url or "http://localhost:11434/api/chat"
+        payload={
+            "model": self.model,
+            "messages": [message.to_dict() for message in messages],
+            "options":{
+                "temperature": temperature,
+            },
+            "format":format,
+            "stream":True
+        }
+        try:
+            response=post(url=url,json=payload,headers=headers,stream=True)
+            response.raise_for_status()
+            chunks=response.iter_lines(decode_unicode=True)
+            for chunk in chunks:
+                yield loads(chunk)
+        except Exception as err:
+            print(err)
+
+class Ollama(BaseInference):
+    def invoke(self, query:str,format:Literal['','json']='')->AIMessage:
+        headers=self.headers
+        temperature=self.temperature
+        url=self.base_url or "http://localhost:11434/api/generate"
+        payload={
+            "model": self.model,
+            "prompt": query,
+            "options":{
+                "temperature": temperature,
+            },
+            "format":format,
+            "stream":False
+        }
+        try:
+            response=post(url=url,json=payload,headers=headers)
+            response.raise_for_status()
+            json_obj=response.json()
+            return AIMessage(json_obj['response'])
+        except Exception as err:
+            print(err)
+
+    def stream(self,query:str,format:Literal['','json']=''):
+        headers=self.headers
+        temperature=self.temperature
+        url=self.base_url or "http://localhost:11434/api/generate"
+        payload={
+            "model": self.model,
+            "prompt": query,
+            "options":{
+                "temperature": temperature,
+            },
+            "format":format,
+            "stream":True
+        }
+        try:
+            response=post(url=url,json=payload,headers=headers,stream=True)
+            response.raise_for_status()
+            chunks=response.iter_lines(decode_unicode=True)
+            for chunk in chunks:
+                yield loads(chunk)
+        except Exception as err:
+            print(err)
