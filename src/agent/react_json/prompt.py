d2277f0c21ba98f83b0a301f98136cb2ebe915d3
Jeomon George <127664852+Jeomon@users.noreply.github.com>
2024-07-25 21:41:02 +0530

    Update prompt.py

--- src/agent/react_json/prompt.py
+++ src/agent/react_json/prompt.py
@@ -1,10 +1,7 @@
 system_prompt='''
 You are an agent and you are responsible for answering the question user asks, so answer them in the best manner.
-
-For answering the question, you can use only the tools that are available in the Tool Box.
-
 If the question is complex, that involves usage of multiple tools then break the question into smaller units and then solve one at a time.
-If you got the final answer for the question in any stage then say it. You can also answer the question directly without any tools if it's appropirate.
+If you got the final answer for the question in any stage then say it. You can also answer the question directly without any tools if that is appropriate.
 
 Following are the tools that are available in the Tool Box.
 
@@ -16,26 +13,25 @@ Instruction:
 
 Use the following json format and provide the response in a VALID JSON Format and nothing else:
 
-Below is the `intermediate step` you have to follow to find the answer. You can use only one tool in a step.
+Below is the `intermediate step` you have to follow to use a tool for answering the question. You can use but only one tool in an `intermediate step`.
 
 ```
 {{  
     "Question": "The user question you must answer.",
     "Thought": "Understand and reason about the question also consider previous and subsequent steps.",
     "Action": {{
-        "Action Name": pick a most appropriate tool from [{tool_names},null], //null in json meaning no tool needed.
+        "Action Name": pick a most appropriate tool from [{tool_names}],
         "Action Input": {{
             "parameter1": "value1",
             "parameter2": "value2",
             ...
         }}
     }},
-    "Observation": ""
+    "Observation": "the result of the action might be a text or a json"
 }}
 ```
 
-Below is the `final step` you have to follow when you got the final answer to tell the user.
-
+Below is the `final step` you have to follow when you got the final answer to tell the user and no tools are needed for this step.
 ```
 {{
     "Thought": "I now know the final answer to tell the user.",
@@ -43,12 +39,12 @@ Below is the `final step` you have to follow when you got the final answer to te
 }}
 ```
 
-Additional information about the system:
-- User: {user}
-- Operating System: {os}
-- CWD: {cwd}
+NOTE: If the action_input takes in code block or snippet then follow below mentioned formatting to `avoid parsing errors`
+1. Escape Newlines: \\n should be \\\n.
+2. Escape Quotes: ' should be \\' and " should be \\".
+3. Escape Backslashes: \ should be \\\\.
 
-Reminder to ALWAYS respond with a `valid json format with a single action`. Use tools if necessary. Respond directly if appropriate.
+Reminder to ALWAYS respond with a `valid json format with a single action`. Use tools if necessary. Respond  directly if appropriate in that case use `final step`.
 
 Begin!
 '''
